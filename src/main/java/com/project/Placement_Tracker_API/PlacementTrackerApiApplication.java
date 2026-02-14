bc595413f63c5a2c709b509b6f3731dd4a222cca
Yash-997 <yashdabhade.official@gmail.com>
2026-02-14 14:52:43 +0530

    Initial commit - setup Spring Boot project

---
+++ src/main/java/com/project/Placement_Tracker_API/PlacementTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.Placement_Tracker_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementTrackerApiApplication.class, args);
+	}
+
+}
